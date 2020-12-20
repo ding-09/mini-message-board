0024f8aa83479013034bcd5c1ec661b994824388
tiffany <yang.ding@hotmail.com>
2020-12-20 14:00:13 -0800

    added link to /new page on button click

--- routes/new.js
+++ routes/new.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/new', (req, res, next) => {
-    res.render('new');
+router.get('/', (req, res, next) => {
+    res.render('form');
 })
+
+module.exports = router;
