930ed88ed8a3f7ee81c4b95daaabdaf672881244
tiffany <yang.ding@hotmail.com>
2020-12-19 20:36:34 -0800

    added default view/messages and styling

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const messages = require('../public/javascripts/messages');
 
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
